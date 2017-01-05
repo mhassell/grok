526394674ab7bf232e5dc1a2d74d096b78f57049
Matthew Hassell <hassell.matthew@gmail.com>
2017-01-04 19:58:52 -0500

    a few more lines of progress

---
+++ LP/LP_inconsistent.py
@@ -0,0 +1,15 @@
+# an LP example from the documentation
+# last modified: January 4, 2017
+
+from scipy.optimize import linprog
+
+c = [-1,2]
+A = [[1,-1],[-1,1]]
+b = [0,0]
+x0_bounds = (0, None)
+x1_bounds = (0, None)
+
+res = linprog(c, A_ub=A, b_ub=b, bounds=(x0_bounds, x1_bounds),
+              options={'disp':True})
+
+print res
