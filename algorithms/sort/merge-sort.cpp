8948eb107d4f59bfc5ec8aabeab6b12cf3f346b8
Matthew Hassell <hassell.matthew@gmail.com>
2017-01-26 20:54:01 -0500

    this took an embarassing amount of debugging

--- algorithms/sort/merge-sort.cpp
+++ algorithms/sort/merge-sort.cpp
@@ -8,9 +8,11 @@ int main(){
 
   std::vector<int> v;
   v = {0,2,4,1,3,5,7};
+  // v = {1,3,5,7,0,2,4};
+
   merge(v,0,2,6);
 
-  for(int j=0; j<7; j++){
+  for(int j=0; j<v.size(); j++){
     std::cout << v[j] << '\n';
   }
 
@@ -20,28 +22,53 @@ void merge(std::vector<int> &v, int p, int q, int r){
 
   int n1 = q-p+1;
   int n2 = r-q;
-  std::vector<int> L(n1+1,0); // initialize to a vector of zeros
-  std::vector<int> R(n2+1,0);
+
+  // std::cout << "n1 = "<< n1 << '\n';
+  // std::cout << "n2 = "<< n2 << '\n';
+  // std::cout << "r = " << r << '\n';
+
+  std::vector<int> L(n1,0); // initialize to a vector of zeros
+  std::vector<int> R(n2,0);
   
   for(int j=0; j<n1; j++){
-    L[j] = v[p+j+1];
+    L[j] = v[p+j];
+    // std::cout << "L[j] = " << L[j] << '\n';
   }
 
   for(int j=0; j<n2; j++){
     R[j] = v[q+j+1];
+    // std::cout << "R[j] = " << R[j] << '\n';
   }
 
   int i=0;
   int j=0;
-  // need more logic here
-  for(int k=p; k<r; k++){
-    if(L[i] <= R[j]){   
-      v[k] = L[i];
-      i++;
+  for(int k=p; k<=r; k++){
+    if(i<n1 & j<n2){ // L and R still have entries
+      if(L[i]<=R[j]){   
+	v[k] = L[i];
+	i++;
+      }
+      else{
+	v[k]=R[j];
+	j++;
+	// std::cout << "R[j]= " << R[j] << '\n';
+	// std::cout << "i = " << i << '\n';
+	// std::cout << "j = " << j << '\n';
+	// std::cout << "k = " << k << '\n';
+      }
     }
-    else{
+    else if(i==n1 & j<n2){  // emptied L out
       v[k]=R[j];
       j++;
+      // std::cout << "R[j]= " << R[j] << '\n';
+    }
+    else if(i<n1 & j==n2){  // emptied R out
+      v[k]=L[i];
+      i++;
+      // std::cout << "L[i]= " << L[i] << '\n';
+      // std::cout << "k = " << k << '\n';
+      // std::cout << "i = " << i << '\n';
+      // std::cout << "j = " << j << '\n';
     }
   }
 }
