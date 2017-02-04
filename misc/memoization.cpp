17f9c3db8929aebf1e299bff25e87358fa783904
Matthew Hassell <hassell.matthew@gmail.com>
2017-02-03 20:06:53 -0500

    just a litte work tonight, but lots of learning

--- misc/memoization.cpp
+++ misc/memoization.cpp
@@ -15,7 +15,7 @@ int factorial(int n){
 
 int factorial_memo(int n){
 
-  static std::map<int,int> memo;    // (key type, mapped type)   (why static??)
+  static std::map<int,int> memo;    // (key type, mapped type)
   
   if(n<=1)
     return n;
@@ -29,25 +29,77 @@ int factorial_memo(int n){
   
 }
 
-int main(){
+int fib_memo(int n){
 
-  std::clock_t t1, t2;
+  static std::map<int,int> fib_map;
+  
+  if(n<=1)
+    return n;
+  if(fib_map.count(n)>0)
+    return fib_map[n];
+  int ret = fib_memo(n-1)+fib_memo(n-2);
+  fib_map[n]=ret;
+  return ret;
 
-  int n = 10;
-  t1=clock();
-  std::cout<< factorial_memo(n) << std::endl;
-  t2=clock();
-  std::cout<< "Elapsed cycles for memoized code: " << t2 << std::endl;
+}
 
-  t1=clock();
-  std::cout<< factorial_memo(n) << std::endl;
-  t2=clock();
-  std::cout<< "Elapsed cycles for second call of memoized code: " << t2 << std::endl;
+int fib(int n){
 
-  t1=clock();
-  std::cout<< factorial_memo(n) << std::endl;
-  t2=clock();
-  std::cout<< "Elapsed cycles for regular code: " << t2 << std::endl;
+  if(n<=1)
+    return n;
+  return fib(n-1)+fib(n-2);
+
+}
+
+int main(int argc, char *argv[]){
+
+  int opt = (int) *argv[1]-48;   // this 48 is great /s
+
+  if(opt==0){
+
+    std::clock_t t;
+
+    int n = 12;
+    t=clock();
+    std::cout<< factorial_memo(n) << std::endl;
+    t=clock()-t;
+    std::cout<< "Elapsed cycles for memoized code: " << t << std::endl;
+
+    t=clock();
+    std::cout<< factorial_memo(n) << std::endl;
+    t=clock()-t;
+    std::cout<< "Elapsed cycles for second call of memoized code: " << t << std::endl;
+
+    t=clock();
+    std::cout<< factorial_memo(n) << std::endl;
+    t=clock()-t;
+    std::cout<< "Elapsed cycles for regular code: " << t << std::endl;
+
+  }
+
+  if(opt==1){
+
+    std::clock_t t;
+    
+    int n=25;
+    t=clock();
+    std::cout << fib_memo(n) << std::endl;
+    t=clock()-t;
+    std::cout<< "Elapsed cycles for memo code: " << t << std::endl;
+
+    n = 40;
+    t=clock();
+    std::cout << fib_memo(n) << std::endl;
+    t=clock()-t;
+    std::cout<< "Elapsed cycles for memo code #2: " << t << std::endl;
+
+    t=clock();
+    std::cout << fib(n) << std::endl;
+    t=clock()-t;
+    std::cout<< "Elapsed cycles for regular code: " << t << std::endl;
+    
+
+  }
   
   return 0;
 
