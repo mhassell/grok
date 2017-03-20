0f776394ccc732610c4e29c25343ece1b5050962
Matthew Hassell <hassell.matthew@gmail.com>
2017-03-20 19:10:42 -0400

    starting to grok this, woo

--- OOP/TICPP/ch3/typeDefNoodling.cpp
+++ OOP/TICPP/ch3/typeDefNoodling.cpp
@@ -14,6 +14,28 @@ typedef struct {
 	char c;
 } newStruct;
 
+// typedef a struct with a (tag?)
+typedef struct newStruct2{
+	int i;
+	char c;
+} newStruct2;
+
+// a deluxe struct
+typedef struct newStruct3{
+	int i;
+	char c;
+	newStruct3* p;
+} newStruct3;
+
+/* 
+// a non-example (the compiler fails)
+typedef struct{
+	int i;
+	char c;
+	newStruct4* p;
+} newStruct4;
+*/
+
 int main(){
 
 	// vanilla stuff
@@ -29,5 +51,38 @@ int main(){
 	b.c = 'b';
 	std::cout << "b.i = " << b.i << std::endl;
 	std::cout << "b.c = " << b.c << std::endl;
+
+	// deluxe typedef'd
+	newStruct2 c;
+	c.i = 3;
+	c.c = 'c'; //hwut
+	std::cout << "c.i = " << c.i << std::endl;
+	std::cout << "c.c = " << c.c << std::endl;
+
+	// deluxe structures
+	newStruct3 d, e;
+	d.i = 4;
+	d.c = 'd';
+	d.p = &e;
+
+	e.i = 5;
+	e.c = 'e';
+	e.p = &d;
+
+	std::cout << "d.i = " << d.i << std::endl;
+	std::cout << "d.c = " << d.c << std::endl;
+	std::cout << "d.p = " << d.p << std::endl;
+
+	std::cout << "e.i = " << e.i << std::endl;
+	std::cout << "e.c = " << e.c << std::endl;
+	std::cout << "e.p = " << e.p << std::endl;
+
+	//hmmm, let's break things
+	std::cout << "e.p->i = " << e.p->i << std::endl;
+	e.p->i+=12;
+	std::cout << "e.p->i+=12 = " << e.p->i << std::endl;
+
+
+
 
 }
